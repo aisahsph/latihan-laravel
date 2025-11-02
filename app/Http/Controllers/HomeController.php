85eec06a3849fe476edc33a7a877b41a8533ac01
aisahsph <142556366+aisahsph@users.noreply.github.com>
2025-11-02 16:53:51 +0700

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('pages.app.home');
+    }
+
+    public function todoDetail()
+    {
+        return view('pages.app.todos.detail');
+    }
+}
+
